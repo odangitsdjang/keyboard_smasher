73e925c506c8f2fd8c47556d92604c91dcce06b1
Chu Jang <david.c.jang@gmail.com>
2017-10-05 23:36:30 -0700

    Improve songs

--- bundle.js
+++ bundle.js
@@ -636,6 +636,8 @@ class BeatMap {
   constructor(options, ctx, songLink) {
     this.options = options;
     this.ctx = ctx;
+    this.quad = [];
+    this.chorus = [];
     // check the start of the songname the get the right beatmap
     // careful changing the folder name, because then this will break!!!
     if (songLink[8] === "C") {
@@ -654,40 +656,42 @@ class BeatMap {
   // chorus section = [[begin, end], [begin, end]]
   makeBeatMap(tracknum) {
     this.songData(tracknum);
-    let current = this.measure;
     const retArr = [];
     let i = 0;
     while (i < this.songLengthSeconds) {
       // subtract 2 : moving at 9 pixels, we want to achieve 540 pixels down
       // and at 60 hz, we can move 540 pixels down per second, so
       // that's why we have a 1 second offset
-
+      let triggered = false;
       // break
       for (let j = 0; j < this.break.length; j++) {
         if  (this.break[j][0]-OFFSET_TO_HIT_USER_AREA <= i && i <= this.break[j][1]-OFFSET_TO_HIT_USER_AREA) {
           while (i <= this.break[j][1] - OFFSET_TO_HIT_USER_AREA) {
-            current += this.measure;
             i += this.measure;
-
           }
           continue;
         }
       }
 
-      // hook
+      // hook (double)
       for (let j = 0; j < this.chorus.length; j++) {
-        if (this.chorus[j][0]-OFFSET_TO_HIT_USER_AREA <= i && i <= this.chorus[j][1]-OFFSET_TO_HIT_USER_AREA) {
-          retArr.push(current -OFFSET_TO_HIT_USER_AREA);
-          current += this.measure/2;
+        while (this.chorus[j][0]-OFFSET_TO_HIT_USER_AREA <= i && i <= this.chorus[j][1]-OFFSET_TO_HIT_USER_AREA) {
+          retArr.push(i - OFFSET_TO_HIT_USER_AREA);
           i += this.measure/2;
-          continue;
+          triggered = true;
+        }
+      }
+      // quad
+      for (let j = 0; j < this.quad.length; j++) {
+        while (this.quad[j][0]-OFFSET_TO_HIT_USER_AREA <= i && i <= this.quad[j][1]-OFFSET_TO_HIT_USER_AREA) {
+          retArr.push(i - OFFSET_TO_HIT_USER_AREA);
+          i += this.measure/4;
+          triggered = true;
         }
       }
 
-      if (current-2 > 0)
-        retArr.push(current-OFFSET_TO_HIT_USER_AREA);
-
-      current += this.measure;
+      if (i-1 > 0 && !triggered)
+        retArr.push(i - OFFSET_TO_HIT_USER_AREA);
       i+= this.measure;
     }
     // inplace shuffle retArr
@@ -716,10 +720,11 @@ class BeatMap {
     if (tracknum === 1) {
       // const songLengthSeconds = this.options.songAudio.duration;
       // for some reason duration returns NaN (I guess it happens too fast)
-      this.songLengthSeconds = 170 - 1;  // subtract 1 to end beatmap 3 seconds earlier
-      this.chorus = [[68,90], [145, 167], [189, 195]];  // find the chorus manually from mp3
+      this.songLengthSeconds = 201 - 1;  // subtract 1 to end beatmap 3 seconds earlier
+      this.chorus = [[20.79, 21.5],[68,90], [145, 167]];  // find the chorus manually from mp3
       this.bpm = 194;
-      this.break = [[0,10]];
+      this.break = [[0,11.7]];
+      // this.quad = [[20.79, 21.5], [49.5,50.5], [197.8,198.8]];
       this.options.speed = 9;
       this.options.songName = "Run Me Dry";
       this.measure = this.increments(this.bpm, 0.5);
@@ -736,7 +741,6 @@ class BeatMap {
       // this.chorus = [[68,90], [145, 167]];
       this.bpm = 156;
       this.options.songName = "Limitless";
-      this.chorus = [];
       this.break = [[5,10]];
       this.options.speed = 18;
       this.measure = this.increments(this.bpm, 2);
